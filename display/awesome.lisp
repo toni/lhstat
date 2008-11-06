561062dd9eb6239551308557d3095dad9a01fda7
toni <tony@irational.org>
2008-11-06 17:33:15 +0000

    displaying separated, for easier adding of multiple displays

---
+++ display/awesome.lisp
@@ -0,0 +1,14 @@
+(defun display_stats (linux) 
+    (setf lhstat_bar (format nil "~A | ~A | ~A | ~A | ~A | ~A" 
+			     (slot-value linux 'cpu_temp_display_max)
+			     (slot-value linux 'load_display_max)
+			     (get-time)
+			     (slot-value linux 'storage_traffic_display_simple)
+			     (slot-value linux 'net_traffic_display)
+			     (slot-value linux 'power_display_simple)))
+  (run-shell-command
+   (format nil "~A ~A ~A" 
+	   "echo \"0 widget_tell mybar tb_all text" lhstat_bar "\" |
+	    'awesome-client'"))
+  ;;   (print (format nil "~A" lhstat_bar))
+  )
