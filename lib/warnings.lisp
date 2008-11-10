28bd7a7084ef2196869055c6406e01832242d46d
toni <tony@irational.org>
2008-11-10 19:15:14 +0000

    0.5.3, audio/visual warnings for law battery status

--- lib/warnings.lisp
+++ lib/warnings.lisp
@@ -1,6 +1,11 @@
 (defun warning_low_battery (linux) 
+  (setf mycmd2 (format nil "~A ~A ~A" 
+                      "echo \"Battery critical ... " 
+                      (slot-value linux 'charge_percentage) "% left. \" | festival --tts"))
   (setf mycmd (format nil "~A ~A ~A" 
-		      "echo \" battery C R I T I C A L ... " 
-		      (slot-value linux 'power_display_warning) "\" | 
+                      "echo \" battery C R I T I C A L ... " 
+                      (slot-value linux 'power_display_warning) "\" | 
 osd_cat -p middle -A center -f -adobe-helvetica-bold-*-*-*-64-*-*-*-*-*-*-* -s 4 -d 2"))
-  (run-shell-command mycmd))
+  (run-shell-command mycmd)
+  (run-shell-command mycmd2))
+
